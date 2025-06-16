f795205b6ebd0717ac098572cfc22d724d08b880
Shrish Singh Sourya <shrishss007@gmail.com>
2025-06-16 21:14:47 +0530

    try 6

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,16 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['lh3.googleusercontent.com'], // Add any other image domains you need
+    unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true, // Temporarily ignore TS errors during build
   },
   eslint: {
     ignoreDuringBuilds: true, // Temporarily ignore ESLint errors during build
+  },
+  experimental: {
+    outputFileTracingRoot: undefined,
   },
   webpack: (config, { isServer }) => {
     // Add any webpack customizations here
