1e7f1e88b8182294396673b67d13ff5dfa9dea24
Shrish Singh Sourya <shrishss007@gmail.com>
2025-06-16 22:56:11 +0530

    try 18

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -8,10 +8,7 @@ const connectDB = async () => {
     }
     
     console.log('Connecting to MongoDB...');
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
