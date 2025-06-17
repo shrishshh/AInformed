750b0626a895187a3b09e3745fb4ed2943157e91
Shrish Singh Sourya <shrishss007@gmail.com>
2025-06-17 20:28:10 +0530

    crash issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable telemetry
-  telemetry: false,
-  
   // Handle build warnings without failing
   typescript: {
     // !! WARN !!
